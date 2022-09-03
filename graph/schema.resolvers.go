0461f991ecbf3ef9fca7338e9b6b50683a5f0da8
Campbell Lowman <campbelljlowman@gmail.com>
2022-09-03 19:06:58 -0400

    channel data structure refactor

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -16,13 +16,22 @@ import (
 
 // CreateSession is the resolver for the createSession field.
 func (r *mutationResolver) CreateSession(ctx context.Context) (*model.Session, error) {
+	sessionID := 81
 	if r.sessions == nil {
 		r.sessions = make(map[int]*model.Session)
 	}
 
+	// Create map on first time - maybe change this to a constructor? 
+	if r.channels == nil {
+		r.channels = make(map[int]chan *model.Session)
+	}
+
+	mc := make(chan *model.Session, 1)
+	r.channels[sessionID] = mc
+
 	// Make session ID random
 	session := &model.Session{
-		ID:               81,
+		ID:               sessionID,
 		CurrentlyPlaying: nil,
 		Queue:            nil,
 	}
@@ -56,9 +65,17 @@ func (r *mutationResolver) UpdateQueue(ctx context.Context, sessionID int, song 
 	sort.Slice(session.Queue, func(i, j int) bool {return session.Queue[i].Votes > session.Queue[j].Votes})
 
 	// Update subscription
-	// TODO: Check if channel exists! 
 	ch := r.channels[sessionID]
 	ch <- session
+
+	select {
+	case ch <- session: // This is the actual send.
+		// Our message went through, do nothing
+	default: // This is run when our send does not work.
+		fmt.Println("Channel closed.")
+		// You can handle any deregistration of the channel here.
+	}
+
 	return session, nil
 }
 
@@ -79,20 +96,9 @@ func (r *queryResolver) Session(ctx context.Context, sessionID *int) ([]*model.S
 
 // SessionUpdated is the resolver for the sessionUpdated field.
 func (r *subscriptionResolver) SessionUpdated(ctx context.Context, sessionID int) (<-chan *model.Session, error) {
-	// Create map on first time - maybe change this to a constructor? 
-	if r.channels == nil {
-		r.channels = make(map[int]chan *model.Session)
-	}
+	channel := r.channels[sessionID]
+	return channel, nil
 
-	channel, channelExists := r.channels[sessionID]
-
-	if(channelExists){
-		return channel, nil
-	}else{
-		mc := make(chan *model.Session, 1)
-		r.channels[sessionID] = mc
-		return mc, nil
-	}
 	// TODO: Cleanup channel?
 }
 
