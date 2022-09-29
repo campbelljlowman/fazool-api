57b213c0aecb341d919e227f0a49ee1262870ad0
Campbell Lowman <campbelljlowman@gmail.com>
2022-09-28 20:48:26 -0400

    reset sessions in db on restart

--- graph/resolver.go
+++ graph/resolver.go
@@ -2,7 +2,9 @@ package graph
 
 //go:generate go get github.com/99designs/gqlgen@v0.17.15 && go run github.com/99designs/gqlgen generate
 import (
+	"fmt"
 	"sync"
+	"context"
 
 	"github.com/campbelljlowman/fazool-api/graph/model"
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -21,6 +23,19 @@ type Resolver struct {
 
 func NewResolver (client *pgxpool.Pool) *Resolver {
 	// TODO: Migrations? Make tables if they don't exist?
+	// Not sure if this is the right place for this but it needs to happen somewhere
+	queryString := fmt.Sprintf(`
+	UPDATE public.user
+	SET session_id = 0
+	`)
+	commandTag, err := client.Exec(context.Background(), queryString)
+	if err != nil {
+		print("Error adding new session to database")
+	}
+	if commandTag.RowsAffected() != 1 {
+		print("No user found to update")
+	}
+	
 	return &Resolver{
 		sessions:			make(map[int]*model.Session),
 		channels:			make(map[int][]chan *model.Session),
